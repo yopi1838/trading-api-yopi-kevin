e033fbfb548d6edbc05531c496c2eec726f55439
yopi1838 <yopi1838@gmail.com>
2020-12-16 12:59:56 +0900

    Connected to GCP Bucket and added Tokyo Timezone

--- lib/get_symbols.py
+++ lib/get_symbols.py
@@ -1,11 +1,19 @@
 import csv
 import os
+from google.cloud import storage
 
 def get_symbols():
-    file_location = r"C:\Users\Yopi-CEC\Desktop\programming\trading-api-yopi\data"
-    with open(os.path.join(file_location,"nasdaqlisted.txt"), "r") as f:
+    #Get API key from Cloud Storage
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('trading-api-ky')
+    
+    #Get Symbol Lists
+    symbol_blob = bucket.blob('nasdaqlisted.txt')
+    symbol_list = symbol_blob.download_to_filename("gcp")
+    
+    with open(os.path.join(symbol_list,"nasdaqlisted.txt"), "r") as f:
         first_column = [row[0] for row in csv.reader(f, delimiter="|")]
-        #print(first_column[1])
+        print(first_column[1])
 
 if __name__ == "__main__":
     get_symbols()
--- lib/get_symbols_data.py
+++ lib/get_symbols_data.py
@@ -2,4 +2,29 @@ import pytz
 import requests
 import pandas as pd
 import numpy as np
+from datetime import datetime, timedelta
 from google.cloud import bigquery
+#import pyarrow
+from google.cloud import storage
+import string
+import time
+
+def daily_equity_quotes(event, context):
+    #Get API key from Cloud Storage
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('trading-api-ky')
+    
+    blob = bucket.blob('secret-key.txt')
+    api_key = blob.download_as_string()
+
+    #Check if the market was open today (weekday).
+    today = datetime.today().astimezone(pytz.timezone("Asia/Tokyo"))
+    today_fmt = today.strftime('%Y-%m-%d')
+
+    #Call td ameritrade hours endpoint for equities to see if it is open
+    market_url = 'https://api.tdameritrade.com/v1/marketdata/EQUITY/hours'
+
+    print(today_fmt)
+
+if __name__ == "__main__":
+    daily_equity_quotes("a","b")
