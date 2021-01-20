cd96899c0acd94455618219cdc7f4f91ce505a5a
yopi1838 <yopi1838@gmail.com>
2021-01-20 13:42:21 +0900

    Change API

--- lib/get_backtest_data.py
+++ lib/get_backtest_data.py
@@ -34,8 +34,8 @@ def chunks(l,n):
     return (l[i:i+k] for i in range(0, len(l), k))
 
 # Only doing one day here as an example
-start_date = datetime.strptime('2019-11-19', '%Y-%m-%d')
-end_date = datetime.strptime('2020-11-19', '%Y-%m-%d')
+start_date = datetime.strptime('2000-01-01', '%Y-%m-%d')
+end_date = datetime.strptime('2020-12-31', '%Y-%m-%d')
 
 # Convert to unix for the API
 start_date_ms = unix_time_millisec(start_date)
@@ -47,7 +47,7 @@ alpha = list(string.ascii_uppercase)
 symbols = []
 
 for each in alpha:
-    url = 'http://eoddata.com/stocklist/NYSE/{}.htm'.format(each)
+    url = 'http://eoddata.com/stocklist/NASDAQ/{}.htm'.format(each)
     resp = requests.get(url)
     site = resp.content
     soup = BeautifulSoup(site, 'html.parser')
@@ -58,34 +58,39 @@ for each in alpha:
 # Remove the extra letters on the end
 symbols_clean = []
 # print(symbols)
-symbols_randomized = random.sample(symbols, 120)
+symbols_randomized = random.sample(symbols, 480)
 
 for each in symbols_randomized:
     each = each.replace('.', '-')
     symbols_clean.append((each.split('-')[0]))
 
-# Get the price history for each stock. This can take a while
-consumer_key = 'BIFGL3BYYNDPGQRLVDA50OOH0OSXVIGR'
-params = {
-    'apikey': consumer_key,
-    'periodType': 'month',
-    'frequencyType': 'daily',
-    'frequency': 1,
-    'startDate': start_date_ms,
-    'endDate': end_date_ms,
-    'needExtendedHoursData': 'true'
-    }
+symbol_aapl = ["AAL"]
 
-sem = asyncio.BoundedSemaphore(1)
-symbl_l, open_l, close_l, volume_l, date_l = [],[],[],[],[]
+# Get the price history for each stock. This can take a while
+consumer_key = 'QUCHJO09418JZSB4'
+
+
+sem = asyncio.BoundedSemaphore(120)
+symbl_l, open_l, close_l, volume_l, date_l, high_l, low_l = [], [], [], [], [], [], []
 
 async def get_pricehistory(symbol, session):
+    params = {
+        'apikey': consumer_key,
+        'function': 'TIME_SERIES_DAILY',
+        'symbol': symbol,
+        'periodType': 'month',
+        'frequencyType': 'daily',
+        'frequency': 1,
+        'startDate': start_date_ms,
+        'endDate': end_date_ms,
+        'needExtendedHoursData': 'true'
+    }
     async with sem:
         url = r"https://api.tdameritrade.com/v1/marketdata/{}/pricehistory".format(symbol)
         try:
             response = await session.request(method='GET', url=url, params=params)
             print(f"Processing {url}...")
-            # await asyncio.sleep(60)
+            await asyncio.sleep(60)
             response.raise_for_status()
             print(f"Response status ({url}: {response.status}")
         except HTTPError as http_err:
@@ -116,12 +121,16 @@ def extract_responses(response):
     for i in range(len(candle)):
         open_price = response.get("candles", [{}])[i]['open']
         close_price = response.get("candles", [{}])[i]['close']
+        high = response.get("candles", [{}])[i]['high']
+        low = response.get("candles", [{}])[i]['low']
         volume = response.get("candles", [{}])[i]['volume']
         date = response.get("candles", [{}])[i]['datetime']
 
         symbl_l.append(symbol)
         open_l.append(open_price)
         close_l.append(close_price)
+        high_l.append(high)
+        low_l.append(low)
         volume_l.append(volume)
         date_l.append(date)
 
@@ -129,14 +138,19 @@ def extract_responses(response):
         'symbol': symbl_l,
         'open': open_l,
         'close': close_l,
+        'high': high_l,
+        'low': low_l,
+        'volume': volume_l,
         'date': date_l
     })
     df['date']=pd.to_datetime(df['date'], unit='ms')
-    df['date'] = df['date'].dt.strftime('%Y-%m-%d, %H-%M-%S')
+    df['date'] = df['date'].dt.strftime('%Y-%m-%d')
     df = df.dropna()
 
     df1 = df[df['symbol'] == symbol]
-    df1.to_csv(f'.\data\{symbol}_pricehistory.csv', index=False)
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(my_path, "../data")
+    df1.to_csv(r"{}/{}_pricehistory.csv".format(path, symbol), index=False)
     return 'Parsed!'
 
 async def run_program(symbol, session):
@@ -153,41 +167,15 @@ async def run_program(symbol, session):
     try:
         response = await get_pricehistory(symbol, session)
         extract_responses(response)
-
-        # # Add to BigQuery
-        # client = bigquery.Client()
-        # dataset_id = "{}.equity_data".format(client.project)
-        # table_id = 'pricehistory_data_{}_2019-11-19'.format(symbol)
-
-        # table_ref = "{}.{}".format(dataset_id,table_id)
-                    
-        # job_config = bigquery.LoadJobConfig()
-        # job_config.source_format = bigquery.SourceFormat.CSV
-        # job_config.autodetect = True
-        # job_config.ignore_unknown_values = True
-
-        # job = client.load_table_from_dataframe(
-        #         df,
-        #         table_ref,
-        #         location="US",
-        #         job_config=job_config
-        #         )
-
-        # job.result()
-        # print(f"Response: {json.dumps(parsed_response, indent=2)}")
-        # return parsed_response
-
     except Exception as err:
         print(f"Exception occurred: {err}")
         pass
 
 async def run_session():
     async with ClientSession() as session:
-        tasks = [run_program(symbol, session) for symbol in symbols_clean]
+        tasks = [run_program(symbol, session) for symbol in symbol_aapl]
         await asyncio.gather(*tasks)
         
-        
-
 def main():
     loop = asyncio.get_event_loop()
     try:
